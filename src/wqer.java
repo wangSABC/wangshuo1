5e5cdc56e2585a099795b51dcd780dd6df670409
wangSABC <wangshuo16@bupt.edu.cn>
2017-03-21 16:28:29 +0800

    王硕

---
+++ src/wqer.java
@@ -0,0 +1,13 @@
+/** 
+* @author  wangshuo
+* @date 创建时间：2017年3月21日 下午4:27:35 
+* 类说明 
+*/
+public class wqer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
